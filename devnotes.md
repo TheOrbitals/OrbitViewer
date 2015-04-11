0a4df7b0d40bc99000be2a9761712093dfa3ea30
Don Smith <locksmithdon@gmail.com>
2015-04-11 15:29:49 +1200

    Added a couple of draft objects and some structure

---
+++ devnotes.md
@@ -0,0 +1,9 @@
+Developer Notes
+===================
+
+## time.js
+
+ - Line 68: Ensure reference "Astro.JD2000"
+ - Line 98: Investigate use of Span (TimeSpan)
+
+
---
+++ src/astro.js
@@ -0,0 +1,8 @@
+/**
+ * Astronomical Constants
+ */
+module.exports = {
+  GAUSS: 0.01720209895,
+  JD2000: 2451545.0, // 2000.1.1 12h ET
+  JD1900: 2415021.0  // 1900.1.1 12h ET
+};
